231bee22358c7a6b98ac018ddeb07134f3badac7
Fawad Malik <fawad.malik@hotmail.com>
2020-02-05 16:58:20 -0800

    Adds starter code for the project. Feature branches will contain updates

---
+++ src/main/java/com/e2eqa/solidwithjava/App.java
@@ -0,0 +1,13 @@
+package com.e2eqa.solidwithjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
